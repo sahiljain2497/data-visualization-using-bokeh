389fb2a34bdb6759b5399ab979765088cfa478e7
Sahil Jain <sahiljainpkl199724@gmail.com>
2018-01-16 00:08:59 +0530

    initial commit

---
+++ bokehuse.py
@@ -0,0 +1,18 @@
+from bokeh.plotting import figure,output_file,show
+import xlrd
+
+o=figure(plot_width=500,plot_height=500)
+book=xlrd.open_workbook("./verlegenhuken.xlsx")
+sheet=book.sheet_by_index(0)
+x=[]
+y=[]
+for i in  range(1,sheet.nrows):
+    for j in range(sheet.ncols-2,sheet.ncols):
+        if j == sheet.ncols-2 :
+            x.append(sheet.cell(i,j).value)
+        else :
+            y.append(int(sheet.cell(i,j).value)/10)
+#print(x,y)
+o.circle(x,y,size=5,color="blue")
+output_file("Graph.html")
+show(o)
